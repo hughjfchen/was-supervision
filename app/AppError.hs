63c3428b9d760aa98d8faffe2aeaa45b2f52295a
hughjfchen <hugh.jf.chen@gmail.com>
2021-03-10 09:29:26 +0800

    wrap MyError into AppError.

--- app/AppError.hs
+++ app/AppError.hs
@@ -7,37 +7,12 @@ module AppError
 import As
 import Core.MyError
 
-data AppError = AppInvalidUrlError Text
+data AppError = MyError MyError
+              | AppInvalidUrlError Text
               | AppHttpError Text
-              | AppNotSecure Text
-              | AppNotFound Text
-              | AppUserNameEmpty
-              | AppPasswordEmpty Text
-              | AppPasswordNotMatch Text
-              | AppUserAlreadyLogined Text
-              | AppPreSessionChangeNotSaved Text
-              | AppChangeNotSaved Text
-              | AppJVMParametersExist Text
               deriving stock (Show, Typeable)
 
 instance As AppError MyError where
-  as (NotSecure url) = AppNotSecure url
-  as (NotFound url) = AppNotFound url
-  as (UserNameEmpty) = AppUserNameEmpty
-  as (PasswordEmpty pwd) = AppPasswordEmpty pwd
-  as (PasswordNotMatch pwd) = AppPasswordNotMatch pwd
-  as (UserAlreadyLogined user) = AppUserAlreadyLogined user
-  as (PreSessionChangeNotSaved user) = AppPreSessionChangeNotSaved user
-  as (ChangeNotSaved user) = AppChangeNotSaved user
-  as (JVMParametersExist jvmpara) = AppJVMParametersExist jvmpara
-  match (AppNotSecure url) = Just $ NotSecure url
-  match (AppNotFound url) = Just $ NotFound url
-  match (AppUserNameEmpty) = Just $ UserNameEmpty
-  match (AppPasswordEmpty pwd) = Just $ PasswordEmpty pwd
-  match (AppPasswordNotMatch pwd) = Just $ PasswordNotMatch pwd
-  match (AppUserAlreadyLogined user) = Just $ UserAlreadyLogined user
-  match (AppPreSessionChangeNotSaved user) = Just $ PreSessionChangeNotSaved user
-  match (AppChangeNotSaved user) = Just $ ChangeNotSaved user
-  match (AppJVMParametersExist jvmpara) = Just $ JVMParametersExist jvmpara
-  match (AppInvalidUrlError _) = Nothing
-  match (AppHttpError _) = Nothing
+  as = MyError
+  match (MyError mye) = Just mye
+  match _ = Nothing
